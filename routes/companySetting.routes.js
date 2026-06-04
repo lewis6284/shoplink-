263e0dfc27bf54ab2af2018cf9ae20a7c08f7bc2
yves <yvart.pro@gmail.com>
2026-06-04 19:27:53 +0200

    add stamp and all in pdf print

--- routes/companySetting.routes.js
+++ routes/companySetting.routes.js
@@ -6,11 +6,12 @@ const upload = require('../utils/upload');
 
 const router = express.Router();
 
+router.get('/', CompanySettingController.getAll);
+router.get('/:id', CompanySettingController.getById);
+
 router.use(authMiddleware);
 router.use(roleMiddleware(['owner']));
 
-router.get('/', CompanySettingController.getAll);
-router.get('/:id', CompanySettingController.getById);
 router.post('/', upload.single('stamp'), CompanySettingController.create);
 router.patch('/:id', upload.single('stamp'), CompanySettingController.update);
 router.delete('/:id', CompanySettingController.delete);
